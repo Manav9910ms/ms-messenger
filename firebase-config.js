626ab7fc896268a0057dac31c6c67599682c3ef5
Manav9910ms <manav991077@gmail.com>
2026-05-07 20:14:52 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB92P1v8_9hPLhgqN5YmUzUXF_IIuD7Al0",
+  authDomain: "ms-messenger-sys.firebaseapp.com",
+  projectId: "ms-messenger-sys",
+  storageBucket: "ms-messenger-sys.firebasestorage.app",
+  messagingSenderId: "761516646845",
+  appId: "1:761516646845:web:a574e6677fcc9f826872d4"
+};
