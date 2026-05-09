ddbb8b479d2577864350c5719776a449bdb25e27
Manav9910ms <manav991077@gmail.com>
2026-05-09 20:50:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ import "./auth.js";
 import "./users.js";
 import "./messages.js";
 import "./presence.js";
+
+const backBtn =
+document.getElementById("backBtn");
+
+if(backBtn){
+
+  backBtn.onclick = ()=>{
+
+    document
+    .getElementById("sidebar")
+    .classList.remove("hide");
+
+    document
+    .getElementById("chatArea")
+    .classList.remove("active");
+
+  };
+
+}
