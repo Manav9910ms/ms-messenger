a11aee0a74da12d361e816126dd1a744d3686c04
Manav9910ms <manav991077@gmail.com>
2026-08-23 00:32:09 +0530

    fix: use project Firebase config for messaging

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,15 @@
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC8FJX7mJ8d9wB8k2o6tN9hQ0a1Bc2De3F",
-  authDomain: "ms-messenger-6f0f6.firebaseapp.com",
-  projectId: "ms-messenger-6f0f6",
-  storageBucket: "ms-messenger-6f0f6.appspot.com",
-  messagingSenderId: "1079307021117",
-  appId: "1:1079307021117:web:4b96f7b2bbd0f0a2d9d9b5"
-};
+firebase.initializeApp({
+  apiKey: "AIzaSyB92P1v8_9hPLhgqN5YmUzUXF_IIuD7Al0",
+  authDomain: "ms-messenger-sys.firebaseapp.com",
+  projectId: "ms-messenger-sys",
+  storageBucket: "ms-messenger-sys.firebasestorage.app",
+  messagingSenderId: "761516646845",
+  appId: "1:761516646845:web:a574e6677fcc9f826872d4"
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
